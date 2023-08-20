e76259b6e181125bd02546aa34b61d7bec2157a9
Anthony <gioant1@hotmail.com>
2023-08-19 23:23:12 -0400

    modified logic to include:
    1) main & footer logic as it will remain constant for each pages
    2) modified event listeners
    3) should now correctly change the content inside the main element instead of creating a new main element when we call functions

--- src/index.js
+++ src/index.js
@@ -1,35 +1,66 @@
 import { createPageLinks } from './pages.js';
 import { createHomePage } from "./homepage.js";
-import { createContact } from "./contact.js";
-import { createMenu } from "./menu.js";
+import { createContactPage } from "./contact.js";
+import { createMenuPage } from "./menu.js";
 
 //create tabbed menu
 createPageLinks();
 
-//create initial page and display homepage content
-createHomePage();
+// //create initial page and display homepage content
+// createHomePage();
+
+// Create main element and append it to the body
+const mainContent = document.createElement('main');
+document.body.appendChild(mainContent);
+
+
+// Function to clear and update main content
+function updateMainContent(newContent) {
+    mainContent.innerHTML = '';
+    mainContent.appendChild(newContent);
+
+    // Add footer to mainContent
+    mainContent.appendChild(createFooter());
+}
 
 //prepare logic for "tab-switching"
-const homeTab = document.getElementById('homepage');
+const homeTab = document.getElementById('homeTab');
 const contactTab = document.getElementById('contactTab');
-const menuTab = document.getElementById('menuTab');
-const mainContent = document.querySelector('main');
+// const menuTab = document.getElementById('menuTab');
 
 
-//event listeners
+
+// Event listeners
 homeTab.addEventListener('click', () => {
-    mainContent.innerHTML = '';
-    createHomePage();
+    updateMainContent(createHomePage());
 });
 
 contactTab.addEventListener('click', () => {
-    mainContent.innerHTML = '';
-    createContactPage();
+    updateMainContent(createContactPage());
 });
 
+// menuTab.addEventListener('click', () => {
+//     updateMainContent(createMenuPageContent());
+// });
 
-menuTab.addEventListener('click', () => {
-    mainContent.innerHTML = '';
-    createMenuPage();
-});
 
+// menuTab.addEventListener('click', () => {
+//     mainContent.innerHTML = '';
+//     createMenuPage();
+// });
+
+// Function to create and return the footer element
+function createFooter() {
+    const footerDiv = document.createElement('footer');
+
+    const footerParagraph = document.createElement('p');
+    footerParagraph.textContent = "Copyright © 2023 Anthony Giolti Funes";
+
+    footerDiv.appendChild(footerParagraph);
+
+    return footerDiv;
+}
+
+
+//intitially load homepage first
+updateMainContent(createHomePage());

f6f57140e514a23ea0507b8ed1292a34b35c8673
Anthony <gioant1@hotmail.com>
2023-08-19 21:30:08 -0400

    did the following:
    1) imported remaining functions from module files (contact & menu JS files)
    2) added logic to "tab switch" between the different pages

--- src/index.js
+++ src/index.js
@@ -1,5 +1,35 @@
 import { createPageLinks } from './pages.js';
 import { createHomePage } from "./homepage.js";
+import { createContact } from "./contact.js";
+import { createMenu } from "./menu.js";
 
+//create tabbed menu
 createPageLinks();
+
+//create initial page and display homepage content
 createHomePage();
+
+//prepare logic for "tab-switching"
+const homeTab = document.getElementById('homepage');
+const contactTab = document.getElementById('contactTab');
+const menuTab = document.getElementById('menuTab');
+const mainContent = document.querySelector('main');
+
+
+//event listeners
+homeTab.addEventListener('click', () => {
+    mainContent.innerHTML = '';
+    createHomePage();
+});
+
+contactTab.addEventListener('click', () => {
+    mainContent.innerHTML = '';
+    createContactPage();
+});
+
+
+menuTab.addEventListener('click', () => {
+    mainContent.innerHTML = '';
+    createMenuPage();
+});
+

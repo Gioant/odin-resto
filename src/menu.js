871cf089f6ee0bcf951fa719da9790d626f027b9
Anthony <gioant1@hotmail.com>
2023-08-19 23:23:24 -0400

    emptying boilerplate code

--- src/menu.js
+++ src/menu.js
@@ -2,30 +2,13 @@
 export function createMenuPage() {
     const body = document.querySelector('body');
 
-    //create main html element
-    const main = document.createElement('main');
+
+    // const footerDiv = document.createElement('footer');
+
+    // const footerParagraph = document.createElement('p');
+    // footerParagraph.textContent = "Copyright © 2023 Anthony Giolti Funes";
+
+    // footerDiv.appendChild(footerParagraph);
 
 
-
-    const footerDiv = document.createElement('footer');
-
-    const footerParagraph = document.createElement('p');
-    footerParagraph.textContent = "Copyright © 2023 Anthony Giolti Funes";
-
-    footerDiv.appendChild(footerParagraph);
-
-    //appending elements
-    body.appendChild(main);
-
-    // Append elements to form
-    form.appendChild(emailLabel);
-    form.appendChild(emailInput);
-    form.appendChild(nameLabel);
-    form.appendChild(nameInput);
-    form.appendChild(submitButton);
-
-    contactSection.appendChild(form);
-    main.appendChild(contactSection);
-
-    main.appendChild(footerDiv);
 }

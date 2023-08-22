082ddb815628a0b62e67aec214668b160eea3c0c
Anthony <gioant1@hotmail.com>
2023-08-22 00:19:10 -0400

    added menu logic

--- src/menu.js
+++ src/menu.js
@@ -1,14 +1,63 @@
+import menuItems from './menuData.js';
 
 export function createMenuPage() {
-    const body = document.querySelector('body');
+    // Create a DocumentFragment to hold the elements
+    const fragment = document.createDocumentFragment();
 
 
-    // const footerDiv = document.createElement('footer');
+    const menuSection = document.createElement('section');
+    menuSection.id = "menu-section";
 
-    // const footerParagraph = document.createElement('p');
-    // footerParagraph.textContent = "Copyright © 2023 Anthony Giolti Funes";
+    const div1 = document.createElement('div');
+    div1.id = "menu-items";
 
-    // footerDiv.appendChild(footerParagraph);
+    const h2 = document.createElement('h2');
+    h2.textContent = "Bakery Items";
 
+    const p = document.createElement('p');
+    p.textContent = "Explore Our Range Of Organic Goodness"
+
+    div1.appendChild(h2);
+    div1.appendChild(p);
+
+    menuSection.appendChild(div1);
+
+
+    const div2 = document.createElement('div');
+    div2.id = "menu";
+
+    // Loop through menu items data and create div elements
+    for (const item of menuItems) {
+        const itemDiv = document.createElement('div');
+        itemDiv.classList.add('menu-item');
+
+        const img = document.createElement('img');
+        img.src = item.image;
+        img.alt = item.name;
+
+        const itemName = document.createElement('h3');
+        itemName.textContent = item.name;
+
+        const itemDescription = document.createElement('p');
+        itemDescription.textContent = item.description;
+
+        const itemPrice = document.createElement('h4');
+        itemPrice.textContent = item.price;
+
+        itemDiv.appendChild(img);
+        itemDiv.appendChild(itemName);
+        itemDiv.appendChild(itemDescription);
+        itemDiv.appendChild(itemPrice);
+
+        div2.appendChild(itemDiv);
+        menuSection.appendChild(div2);
+    }
+
+
+
+    // Append contact section to the fragment
+    fragment.appendChild(menuSection);
+
+    return fragment;
 
 }
